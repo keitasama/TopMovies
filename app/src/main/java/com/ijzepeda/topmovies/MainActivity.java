41ea756c7f8faedd57cd3428f9cabd1198f2711a
Ivan <ilo_ol@hotmail.com>
2015-11-23 02:12:37 -0800

    includes the path for popular movies to the api, just create a method that calls the api and fill a gridlayout. Use the one in th esofttek demo for the grid, and evaluate the width and the orientation!

--- app/src/main/java/com/ijzepeda/topmovies/MainActivity.java
+++ app/src/main/java/com/ijzepeda/topmovies/MainActivity.java
@@ -22,10 +22,11 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Hello Project from gitHub", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Hello Project from gitHub, and api root", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
+//        http://api.themoviedb.org/3/movie/popular?api_key=
     }
 
     @Override
